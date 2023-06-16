597072f164bb3cb7b2d92acd8f407f475bbe0b13
wnstjq0915 <wnstjq0915@gmail.com>
2023-06-16 17:25:13 +0900

    config.py 빼고 다 커밋

--- app.py
+++ app.py
@@ -5,10 +5,19 @@
 # 실행
 # flask run 
 # python app.py
+# 저장하고 하는거 주의
 
 from flask import Flask
-
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource
 app = Flask(__name__)
+api = Api(app)
+
+
+# 경로와 API 동작코드(Resource)를 연결한다.
+api.add_resource(RecipeListResource, '/recipes') # url 뒤에 붙은 /recipes
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+
 
 if __name__ == '__main__':
     app.run()
